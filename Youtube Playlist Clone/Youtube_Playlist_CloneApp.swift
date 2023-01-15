f852dfa0995ae29cdefd5e1b6211658f671c5365
Aaryan Baral <aryanbaral1100@gmail.com>
2023-01-15 11:50:29 +0545

    initial commit

---
+++ Youtube Playlist Clone/Youtube_Playlist_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Youtube_Playlist_CloneApp.swift
+//  Youtube Playlist Clone
+//
+//  Created by Aaryan Baral on 15/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Youtube_Playlist_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
